28e7a5fc444d93bbb0e1623ff6e9eaeb5d1d88ee
jo <josehehall98@gmail.com>
2024-12-05 23:51:19 -0700

    CharacterService

--- client/src/services/CharecterService.ts
+++ client/src/services/CharecterService.ts
@@ -1,21 +1,28 @@
 import { AppState } from "../AppState"
+import { Character } from "../models/Charecter"
+import { logger } from "../utils/Logger"
+import { RMApi } from "./AxiosService"
 
 type CharacterResponse = {
   results: CharacterData[]
+  total_pages: number
+  total_results: number
+  info: number
+  pages: number
 }
 class CharacterService {
-  async changeSearchPage(pageNumber, characterQuery) {
-    const response = await rickAndMortyApi.get(`api/character/?page=${pageNumber}&name=${characterQuery}`)
-    AppState.characterQuery = characterQuery
-    this.handleResponseData(response.data)
-  }
+  // async changeSearchPage(pageNumber: string, characterQuery: string) {
+  //   const response = await RMApi.get(`api/character/?page=${pageNumber}&name=${characterQuery}`)
+  //   AppState.characterQuery = characterQuery
+  //   this.handleResponseData(response.data)
+  // }
 
-  async searchCharacter(characterQuery) {
-    const response = await rickAndMortyApi.get(`api/character/?name=${characterQuery}`)
-    console.log('SEACRHED character 🔍', response.data.results);
-    AppState.characterQuery = characterQuery
-    this.handleResponseData(response.data)
-  }
+  // async searchCharacter(characterQuery: string) {
+  //   const response = await RMApi.get(`api/character/?name=${characterQuery}`)
+  //   console.log('SEACRHED character 🔍', response.data.results);
+  //   AppState.characterQuery = characterQuery
+  //   this.handleResponseData(response.data)
+  // }
 
   clearCharacter() {
     AppState.character = []
@@ -27,37 +34,38 @@ class CharacterService {
     AppState.characterQuery = ''
   }
 
-  async changePage(pageNumber) {
+  // async changePage(pageNumber: string) {
 
-    const response = await rickAndMortyApi.get(`api/character/?page=${pageNumber}`)
-    console.log('CHANGED PAGE 🧍', response.data);
-    this.handleResponseData(response.data)
+  //   const response = await RMApi.get(`api/character/?page=${pageNumber}`)
+  //   console.log('CHANGED PAGE 🧍', response.data);
+  //   this.handleResponseData(response.data)
 
-  }
+  // }
 
 
   async getAllCharacter() {
-    const response = await rickAndMortyApi.get('api/character')
+    const response = await RMApi.get('api/character')
 
     AppState.character = response.data.results
     logger.log("ALL CHARACTER'S", response.data.results)
     AppState.totalPages = response.data.info.pages
   }
 
-  async getCharacterById(characterId) {
-    AppState.characterById = null
-    const response = await rickAndMortyApi.get(`api/character/${characterId}`)
-    AppState.characterById = response.data
+  async getCharacterById(characterId: string) {
+    if (AppState.activeCharacter?.id == characterId) { return }
+    AppState.activeCharacter = null
+    const response = await RMApi.get(`api/character/${characterId}`)
+    AppState.activeCharacter = response.data
     logger.log(response.data)
 
   }
 
-  handleResponseData(responseData) {
-    const newCharacters = responseData.results.map(characterPOJO => new Character(characterPOJO));
-
-    AppState.character = newCharacters;
-    AppState.totalPages = responseData.info.pages;
-  }
+  // handleResponseData(responseData: CharacterResponse) {
+  //   const characters = responseData.results.map((characterData: CharacterData) => new Character(characterData))
+  //   AppState.character = characters
+  //   AppState.totalPages = responseData.pages
+  //   AppState.totalResults = responseData.total_results
+  // }
 }
 
 
